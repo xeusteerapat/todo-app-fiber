40c09922e470c633b73c345aa4388a99b412dae9
xeusteerapat <xeus085@gmail.com>
2021-04-07 18:57:17 +0700

    [EDIT] update create todo

--- server.go
+++ server.go
@@ -35,10 +35,37 @@ func main() {
 	})
 
 	app.Get("/todos", getTodos)
+	app.Post("/todo", createTodo)
 
 	app.Listen(":5000")
 }
 
 func getTodos(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(todos)
+}
+
+func createTodo(c *fiber.Ctx) error {
+	type request struct {
+		Name      string `json:"name"`
+		Completed bool   `json:"completed"`
+	}
+
+	var body request
+
+	err := c.BodyParser(&body)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Bad Request",
+		})
+	}
+
+	todo := Todo{
+		ID:        len(todos) + 1,
+		Name:      body.Name,
+		Completed: body.Completed,
+	}
+
+	todos = append(todos, todo)
+
+	return c.Status(fiber.StatusCreated).JSON(todo)
 }
