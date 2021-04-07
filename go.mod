df9a2f9de2b9947da8e2634778647e282c6b232b
xeusteerapat <xeus085@gmail.com>
2021-04-07 18:49:47 +0700

    [EDIT] update get todos

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/xeusteerapat/todo-app-fiber
+
+go 1.16
+
+require (
+	github.com/cosmtrek/air v1.21.2 // indirect
+	github.com/creack/pty v1.1.11 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/gofiber/fiber/v2 v2.7.1 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/pelletier/go-toml v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
+)
--- server.go
+++ server.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Completed bool   `json:"conpleted"`
+}
+
+var todos = []Todo{
+	{
+		ID: 1, Name: "Learn GO", Completed: true,
+	},
+	{
+		ID: 2, Name: "Learn Rust", Completed: false,
+	},
+	{
+		ID: 3, Name: "Learn Typescript", Completed: true,
+	},
+	{
+		ID: 4, Name: "Build Serverless APP", Completed: false,
+	},
+}
 
 func main() {
-	fmt.Println("Hello, from Fiber")
+	app := fiber.New()
+	app.Use(logger.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!!!!!!!")
+	})
+
+	app.Get("/todos", getTodos)
+
+	app.Listen(":5000")
+}
+
+func getTodos(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusOK).JSON(todos)
 }
